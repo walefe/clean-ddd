80adcb0479fd8efceb4c9f0715a7a40e08657e43
Walefe <walefedev@gmail.com>
2023-08-01 16:40:45 -0300

    chore: mapped properties and create new abstractions

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,25 @@
 import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
-  slug: Slug
   content: string
-  authorId: string
+  slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {}
+export class Question extends Entity<QuestionProps> {
+  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date(),
+    }, id)
+
+    return question
+  }
+}
