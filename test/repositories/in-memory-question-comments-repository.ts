eb943ad3ebb1438ac02ff77a2b8707a9147d0bc5
Walefe <walefedev@gmail.com>
2023-08-07 15:17:07 -0300

    feat: use case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -9,6 +10,14 @@ export class InMemoryQuestionCommentsRepository
   async findById(id: string) {
     const questionComment = this.items.find((item) => item.id.toString() === id)
     if (!questionComment) return null
+
+    return questionComment
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComment = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
 
     return questionComment
   }
