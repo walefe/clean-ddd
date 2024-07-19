10c191dd3726f518e53d9551015ae02826d026b7
Walefe <walefedev@gmail.com>
2024-07-19 13:23:13 -0300

    feat: create domain events and agregated root

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,11 @@ export abstract class Entity<Props> {
     this._id = id ?? new UniqueEntityID()
     this.props = props
   }
+
+  public equals(entity: Entity<any>) {
+    if (entity === this) return true
+    if (entity.id === this._id) return true
+
+    return false
+  }
 }
